b4cfcfeee3d87d61cf694eaf43711278509a545f
IrinaAlcala <58370677+IrinaAlcala@users.noreply.github.com>
2020-12-06 18:29:12 -0800

    added food and grrid

--- game.js
+++ game.js
@@ -1,25 +1,45 @@
-import { SNAKE_SPEED } from './snake.js'
-
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateFood, draw as drawFood } from './food.js'
+import { outsideGrid } from './grid.js'
 
 let lastRenderTime = 0
-// const SNAKE_SPEED = 2
+let gameOver = false
+const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
-    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+  if (gameOver) {
+    if (confirm('You lost. Press ok to restart.')) {
+      window.location = '/'
+    }
+    return
+  }
 
-    console.log('Render')
-    lastRenderTime = currentTime
-    update()
-    draw()
+
+  window.requestAnimationFrame(main)
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+
+  lastRenderTime = currentTime
+
+  update()
+  draw()
 }
 
 window.requestAnimationFrame(main)
 
 function update() {
-updateSnake()
+  updateSnake()
+  updateFood()
+  checkDeath()
 }
 
 function draw() {
-drawSnake(gameBoard)
+  gameBoard.innerHTML = ''
+  drawSnake(gameBoard)
+  drawFood(gameBoard)
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
